981b3b2c2e75cbed65086deb3ad6fe88735ef8e2
Xenetics <xenetics@live.com>
2014-12-06 07:20:22 -0500

    Goblin AI underway
    flee when alone
    seek each other when too far
    seek player when grouped

---
+++ Assets/Scripts/Enemy/FighterBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FighterBehaviour : SteeringBehaviour 
+{
+    public Sensor RightSensor;
+    public Sensor LeftSensor;
+
+    public float sensorRange = 3;
+
+    public float wanderTime = 0.3f;
+    float wanderTimeActual;
+    bool ShouldWander = true;
+
+    void Awake()
+    {
+        wanderTimeActual = wanderTime;
+    }
+
+	void FixedUpdate () 
+    {
+        ShouldIWander();
+
+        AdvancedCollisions(LeftSensor ,RightSensor);
+        
+        ApplySteering();
+        Reset();
+	}
+
+    void ShouldIWander()
+    {
+        if (ShouldWander) // wanter when not turning
+        {
+            Wander();
+            ShouldWander = false;
+        }
+        else
+        {
+            wanderTimeActual -= Time.deltaTime;
+        }
+
+        if (wanderTimeActual <= 0.0f) // resets time of next wander
+        {
+            wanderTimeActual = wanderTime;
+            ShouldWander = true;
+        }
+    }
+}
--- Assets/Scripts/Enemy/GoblinBehaviour.cs
+++ Assets/Scripts/Enemy/GoblinBehaviour.cs
@@ -3,27 +3,67 @@ using System.Collections;
 
 public class GoblinBehaviour : SteeringBehaviour
 {
+    public Sensor LeftSensor;
+    public Sensor RightSensor;
+
+    public Sensor AgroSensor;
+
     public float wanderTime = 0.3f;
-    float wanderTimeActual;
-    bool ShouldWander = true;
-    bool isHolding = false;
-    GameObject held;
+    private float wanderTimeActual;
+
+    public float seperation = 1;
+    private bool isAlone = true;
+    private bool ShouldWander = true;
+    private bool Agro = false;
+    private GameObject partner;
 
     void Awake()
     {
         wanderTimeActual = wanderTime;
     }
 
+    void Update()
+    {
+
+    }
+
     void FixedUpdate()
     {
-        ShouldIWander();
+        WhatDo();
+
+        AdvancedCollisions(LeftSensor, RightSensor);
 
         ApplySteering();
         Reset();
     }
 
-    void ShouldIWander()
+    void WhatDo()
     {
+        if (AgroSensor.isColliding)
+        {
+            GameObject temp = AgroSensor.GiveTarget();
+            if (temp.tag == "Player" && isAlone)
+            {
+                Flee(temp.transform.position);
+                ShouldWander = false;
+            }
+            else if (temp.tag == "Player" && !isAlone)
+            {
+                Seek(temp.transform.position, 1.5f);
+            }
+            else if (temp.tag == "Goblin" && partner == null)
+            {
+                isAlone = false;
+                partner = temp;
+            }
+        }
+
+        if(partner != null && Vector3.Distance(transform.position, partner.transform.position) > 5)
+        {
+            ShouldWander = false;
+            Seek(partner.transform.position, seperation);
+        }
+
         if (ShouldWander) // wanter when not turning
         {
             Wander();
@@ -32,32 +72,18 @@ public class GoblinBehaviour : SteeringBehaviour
         else
         {
             wanderTimeActual -= Time.deltaTime;
+            if (wanderTimeActual <= 0.0f) // resets time of next wander
+            {
+                wanderTimeActual = wanderTime;
+                ShouldWander = true;
+            }
         }
 
-        if (wanderTimeActual <= 0.0f) // resets time of next wander
-        {
-            wanderTimeActual = wanderTime;
-            ShouldWander = true;
-        }
+
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Box" && isHolding == false)
-        {
-            other.transform.parent = this.transform;
-            held = other.gameObject;
-            held.name = "BoxHeld";
-            isHolding = true;
-        }
-        else if (other.gameObject.name == "Box" && isHolding == true && held != other.gameObject)
-        {
-            //held.name = "Box";
-            held.transform.parent = null;
-            //other.transform.parent = this.transform;
-            //other.name = "BoxHeld";
-            //held = other.gameObject;
-            isHolding = false;
-        }
+
     }
 }

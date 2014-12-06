65c5f85d12a8bf841cd7715fd5b5b8d6569f99f1
Xenetics <xenetics@live.com>
2014-12-06 14:36:23 -0500

    Enemy hp bars yeahhhh

---
+++ Assets/Scripts/Enemy/BillBoard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillBoard : MonoBehaviour 
+{
+    public GameObject entity;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 temp = new Vector3(entity.transform.position.x, entity.transform.position.y + 1, entity.transform.position.z + 2);
+        transform.LookAt(temp, Vector3.up);
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemyHPBar.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHPBar : MonoBehaviour 
+{
+    public GameObject entity;
+    public GameObject hpQuad;
+    private float hitPoints = 0;
+    private float maxHitPoints = 0;
+    float normalisedHealth;
+
+
+    void Awake()
+    {
+        maxHitPoints = entity.GetComponent<EnemyStats>().maxHP;
+    }
+
+	void Start () 
+    {
+        hitPoints = entity.GetComponent<EnemyStats>().hp;
+	}
+	
+	void Update () 
+    {
+        hitPoints = entity.GetComponent<EnemyStats>().hp;
+
+        if (hitPoints < 0)
+        {
+            hitPoints = 0;
+        }
+
+        normalisedHealth = (float)hitPoints / maxHitPoints;
+
+        hpQuad.transform.localScale = new Vector3(normalisedHealth, hpQuad.transform.localScale.y, hpQuad.transform.localScale.z);
+
+        if (normalisedHealth <= 0.25f)
+        {
+            hpQuad.renderer.material.color = Color.red;
+        }
+        else if (normalisedHealth <= 0.75f)
+        {
+            hpQuad.renderer.material.color = Color.yellow;
+        }
+        else
+        {
+            hpQuad.renderer.material.color = Color.green;
+        }
+	}
+}

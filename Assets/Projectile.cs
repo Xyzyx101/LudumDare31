89015e9d9902ef1dd63756a151cfa8fbbd7f22fc
Andrew Perrault <andrew.perrault@yahoo.ca>
2014-12-07 13:10:20 -0500

    Dagger added

---
+++ Assets/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float speed = 5f;
+	public float rotationSpeed = 0f;
+	public float damage = 2f;
+	public GameObject sprite;
+	public float maxTimer = 2.0f;
+
+	void Start () {	
+	}
+	
+	void Update () {
+		maxTimer -= Time.deltaTime;
+		if (maxTimer < 0) {
+			Destroy (this.gameObject);
+		}
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		sprite.transform.Rotate (Vector3.forward, rotationSpeed * Time.deltaTime);
+	}
+	
+	void OnCollisionEnter (Collision collision) {
+		collision.other.gameObject.SendMessageUpwards("DoDamage", damage, SendMessageOptions.DontRequireReceiver);
+		Destroy (this.gameObject);
+	}	
+}
---
+++ Assets/ProjectileWeapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileWeapon : MonoBehaviour {
+	public float activeTime = 0.5f;
+	public GameObject projectile;
+	private float shootTime;
+	private bool canShoot;
+	
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive(false);
+		canShoot = true;
+	}
+	
+	void OnEnable () {
+		shootTime = activeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shootTime -= Time.deltaTime;
+		if (canShoot) {
+			Instantiate(projectile, transform.position, transform.rotation);
+			canShoot = false;
+		}
+		if (shootTime < 0) {
+			canShoot = true;
+			gameObject.SetActive(false);
+		}
+	}	
+}

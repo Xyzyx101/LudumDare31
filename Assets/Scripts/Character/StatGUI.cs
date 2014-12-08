b517ca3a5e24e5d878bb15e7be97dd36b6c91bea
Xenetics <xenetics@live.com>
2014-12-07 19:47:47 -0500

    rename file

--- Assets/Scripts/Character/StatGUI.cs
+++ Assets/Scripts/Character/StatGUI.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StateGUI : MonoBehaviour 
+public class StatGUI : MonoBehaviour 
 {
     public Texture2D image;
 
@@ -12,13 +12,10 @@ public class StateGUI : MonoBehaviour
         GUI.depth = 1;
         GUILayout.BeginArea(new Rect(Screen.width * 0.01f, Screen.height * 0.2f, Screen.width * 0.2f, Screen.height * 0.2f));
         
-
         GUI.color = Color.black;
         GUI.DrawTexture(new Rect(Screen.width * 0.01f - 2, (Screen.height * 0.13f), (Screen.width * 0.19f) + 4, (Screen.height * 0.05f) + 4), image);
         GUI.color = Color.white;
 
-        GUI.skin = Skin;
-        GUI.Label(new Rect(Screen.width * 0.01f - 2, (Screen.height * 0.13f), (Screen.width * 0.19f) + 4, (Screen.height * 0.05f) + 4), "Health");
         GUILayout.EndArea();
     }
 

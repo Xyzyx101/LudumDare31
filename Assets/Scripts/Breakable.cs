f00c0325c5b487a9c31b1789ee754a4bd3fbec5e
AlienJim <burton.alexander102@gmail.com>
2014-12-07 21:54:41 -0500

    Added Stuff

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breakable : MonoBehaviour {
+	public GameObject[] Spawnable;
+	public int chanceToSpawn = 5; 
+
+	public void DoDamage(float damage)
+	{
+		Debug.Log ("smashy Smashy");
+		if(Random.Range(0, 100) < chanceToSpawn)
+		{
+			int objectToSpawn = Random.Range(0, Spawnable.Length); 
+			GameObject spawnedObject = (GameObject)Instantiate(Spawnable[objectToSpawn], this.transform.position, Spawnable[objectToSpawn].transform.rotation);
+		}
+
+		Destroy (this.gameObject);
+	}
+}
